de3b7ca0c8b37c8fcaa007d0c47040af32507fd6
Kevin Peek <kevpeek@gmail.com>
2020-02-18 21:04:12 -0500

    basic pomo implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::time::{Instant, Duration};
+use std::thread;
+use std::ops::{Add, Sub};
+
+use notify_rust::Notification;
+
+const POMO_LENGTH: Duration = Duration::from_secs(25 * 60);
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting...");
+
+    let start = Instant::now();
+    let end = start.add(POMO_LENGTH);
+
+    while Instant::now() < end {
+        let remaining = end.sub(Instant::now());
+        println!("Remaining: {}.", format(remaining));
+        thread::sleep(POMO_LENGTH / 25);
+    }
+
+    Notification::new()
+        .summary("Done")
+        .body("Pomodoro complete")
+        .show()
+        .unwrap();
+}
+
+fn format(d: Duration) -> String {
+    let minutes = (d.as_secs() / 60).to_string();
+    let seconds = (d.as_secs() % 60).to_string();
+    return format!("{} minutes, {} seconds", minutes, seconds);
 }
