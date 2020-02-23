11a8355196c5d9d64e81912911db6ffae45efb65
Kevin Peek <kevpeek@gmail.com>
2020-02-23 15:17:31 -0500

    clean up alert sound code and flesh out readme

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# pomo
-A simple Pomodoro timer
+# Pomo
+A simple Pomodoro timer.
 
 ![Rust](https://github.com/kevpeek/pomo/workflows/Rust/badge.svg)
+
+## Installation
+
+Install using Cargo:
+
+`cargo install --force --path .`
+
+## Usage
+
+For a 25 minute Pomodoro with 5 minute break, simply run:
+
+`pomo`
+
+```
+USAGE:
+    pomo [OPTIONS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -d, --duration <duration>    The duration of the pomodoro, in minutes. [default: 25]
+```
--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,10 @@ use std::time::Duration;
 
 use std::fs::File;
 use std::io::BufReader;
-use rodio::Source;
-use std::thread;
+use rodio::{Sink};
 
 
 pub fn notify(title: &str, message: &str) {
-    let device = rodio::default_output_device().unwrap();
-    let file = File::open("/Users/kevinpeek/Downloads/beep.ogg").unwrap();
-    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-    rodio::play_raw(&device, source.convert_samples());
-
     let notification_command = format!("display notification \"{}\" with title \"{}\"", title, message);
     Command::new("osascript")
         .arg("-e")
@@ -26,11 +20,24 @@ pub fn notify(title: &str, message: &str) {
         .output()
         .expect("failed to execute process");
 
-    thread::sleep(Duration::from_secs(4));
+    let device = rodio::default_output_device().unwrap();
+    let sink = Sink::new(&device);
+    let file = File::open("/Users/kevinpeek/Downloads/beep.ogg").unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+    sink.append(source);
+    sink.sleep_until_end();
 }
 
 
 pub fn format(d: Duration) -> String {
     let minutes = d.as_secs() as f64 / 60.0;
     return format!("{:.0} minutes", minutes);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sandbox() {
+
+    }
 }
