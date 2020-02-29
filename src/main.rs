94469c106d58a648242f842c3a17397c7cd14794
Kevin Peek <kevpeek@gmail.com>
2020-02-29 14:55:17 -0500

    use different syles of bar for pomo and break

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::ops::Add;
 use std::time::{Duration, Instant};
 use std::{env, process, thread};
 
+const POMO_BAR_STYLE: &str = "{wide_bar:50.cyan/yellow} {eta}";
+const BREAK_BAR_STYLE: &str = "{wide_bar:50.green/yellow} {eta}";
+
 fn main() {
     let configuration = build_config();
     execute_pomo(configuration.duration());
@@ -25,7 +28,7 @@ fn execute_pomo(pomo_duration: Duration) {
         pomo_duration.as_secs() / 60
     );
 
-    wait_with_progress_bar(pomo_duration);
+    wait_with_progress_bar(pomo_duration, POMO_BAR_STYLE);
     pomo::notify("Pomodoro finished.", "Done");
 }
 
@@ -37,19 +40,19 @@ fn execute_break(pomo_duration: Duration) {
         break_duration.as_secs() / 60
     );
 
-    wait_with_progress_bar(break_duration);
+    wait_with_progress_bar(break_duration, BREAK_BAR_STYLE);
     pomo::notify("Break finished.", "Done");
 }
 
 // wait_with_progress_bar causes the thread to sleep for the specified duration while
 // updating a progress bar to reflect the progress of the wait.
-fn wait_with_progress_bar(duration: Duration) {
+fn wait_with_progress_bar(duration: Duration, style: &str) {
     let start = Instant::now();
     let end = start.add(duration);
     let bar = ProgressBar::new(100);
     bar.set_style(
         ProgressStyle::default_bar()
-            .template("{bar:100.cyan/blue} {eta}")
+            .template(style)
             .progress_chars("#>-"),
     );
     bar.tick(); // force the bar to show
